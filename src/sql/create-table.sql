e753a89551c344c32a32fdb20573b2480b3c6739
Brad <broswen@gmail.com>
2021-07-18 15:48:07 -0500

    update postgres table schema

--- src/sql/create-table.sql
+++ src/sql/create-table.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS public.clickevents
 (
-    id serial NOT NULL,
+    id bigint NOT NULL,
     slug text NOT NULL,
     key text NOT NULL,
     "timestamp" timestamp with time zone NOT NULL,
     ip text NOT NULL,
-    lang text NOT NULL,
-    useragent text NOT NULL,
+    lang text,
+    useragent text,
     PRIMARY KEY (id)
 );
 
